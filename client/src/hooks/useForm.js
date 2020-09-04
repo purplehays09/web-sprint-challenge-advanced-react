29126c0523ca874cb70a5a84711d8565a7a58bd7
david hays <purplehays527@gmail.com>
2020-09-04 09:36:14 -0600

    I think that the custom hook looks good. This moving on to the plant list

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,14 @@
 // write your custom hook here to control your checkout form
+import {useState, useEffect} from 'react'
+
+export  const useForm = () => {
+    const [values, setValues] = useState([])
+
+    const handleChanges = e => {
+        setValues([
+          ...values, e.target.value
+        ]);
+      };
+
+      return [values, handleChanges]
+}
