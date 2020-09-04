89c7bba15025a215776c0937746d9bc252226a9e
david hays <purplehays527@gmail.com>
2020-09-04 10:41:36 -0600

    everything is working but the submit button on the test so far

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -17,4 +17,22 @@ test("form header renders", () => {
     expect(heading).toBeInTheDocument()
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+
+    const wrapper = render(<CheckoutForm/>)
+
+    fireEvent.change(screen.getByLabelText(/first name:/i), {target:{value:'David'}});
+    fireEvent.change(screen.getByLabelText(/last name:/i), {target:{value:'Hays'}});
+    fireEvent.change(screen.getByLabelText(/address:/i), {target:{value:'4879 Somerset Dr.'}});
+    fireEvent.change(screen.getByLabelText(/city:/i), {target:{value:'Salem'}});
+    fireEvent.change(screen.getByLabelText(/state:/i), {target:{value:'UT'}});
+    fireEvent.change(screen.getByLabelText(/zip:/i), {target:{value:'84321'}});
+
+
+    fireEvent.click(screen.getByText(/checkout/i));
+
+
+    const successMessage = wrapper.queryByTestId('successMessage')
+
+    expect(successMessage_.toBeInTheDocument())
+});
