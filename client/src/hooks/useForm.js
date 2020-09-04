01eb684cfcc84b4a2790cd0236eb4d71d5260f3f
david hays <purplehays527@gmail.com>
2020-09-04 10:04:26 -0600

    The checkout form seems to be working and passing the values of state around just fine

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,14 +1,26 @@
 // write your custom hook here to control your checkout form
 import {useState, useEffect} from 'react'
 
-export  const useForm = () => {
-    const [values, setValues] = useState([])
+export  const useForm = (initialValue) => {
+    // const [values, setValues] = useState([])
 
-    const handleChanges = e => {
-        setValues([
-          ...values, e.target.value
-        ]);
+    // const handleChanges = e => {
+    //     setValues([
+    //       ...values, e.target.value
+    //     ]);
+    //   };
+
+      const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+      const [values, setValues] = useState(initialValue);
+    
+      const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
       };
 
-      return [values, handleChanges]
+      return [values, showSuccessMessage, handleChanges, handleSubmit]
 }
