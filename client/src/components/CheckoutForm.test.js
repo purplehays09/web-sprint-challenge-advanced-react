4195acb62ebfe5049c5102fe7c39f41a9e1b3c82
david hays <purplehays527@gmail.com>
2020-10-02 09:41:45 -0600

    The final assertion does not seem to be working but I do not know why

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -18,4 +18,24 @@ test("form header renders", () => {
 
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+    //Arrange
+    const wrapper = render(<CheckoutForm />)
+    const button = wrapper.getByRole('button')
+    console.log(wrapper.debug)
+    const successMessage = wrapper.queryByTestId('successMessage')
+
+    //Act
+    fireEvent.change(screen.getByLabelText(/first name:/i), {target:{value:'David'}});
+    fireEvent.change(screen.getByLabelText(/last name:/i), {target:{value:'Hays'}});
+    fireEvent.change(screen.getByLabelText(/address:/i), {target:{value:'4879 Somerset Dr.'}});
+    fireEvent.change(screen.getByLabelText(/city:/i), {target:{value:'Salem'}});
+    fireEvent.change(screen.getByLabelText(/state:/i), {target:{value:'UT'}});
+    fireEvent.change(screen.getByLabelText(/zip:/i), {target:{value:'84321'}});
+
+    fireEvent.click(button);
+
+    //Assert
+    expect(await successMessage).toBeInTheDocument()
+
+});
