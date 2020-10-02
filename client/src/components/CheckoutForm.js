1bc7d1912f99f147a59d5c7eda8d805d0c324831
david hays <purplehays527@gmail.com>
2020-10-02 09:15:13 -0600

    I reset everything to normal so that I can do it again

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,5 +1,4 @@
-import React from "react";
-import {useForm} from '../hooks/useForm'
+import React, { useState } from "react";
 
 const initialValue = {
   firstName: "",
@@ -15,8 +14,17 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [values, showSuccessMessage, handleChanges, handleSubmit] = useForm(initialValue)
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, setValues] = useState(initialValue);
 
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
 
   return (
     <>
