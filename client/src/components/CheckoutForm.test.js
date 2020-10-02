f1ca4861d7111502ea51a26eabbc945e1ca87697
david hays <purplehays527@gmail.com>
2020-10-02 09:35:12 -0600

    The first test passed just fine, on to the next more hairy one

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render,fireEvent,screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+
+    //Arrange
+    const wrapper = render(<CheckoutForm/>)
+
+    //Act
+    const header = wrapper.getByText(/checkout form/i)
+
+    //Assert
+    expect(header).toBeInTheDocument()
+
+
+});
 
 test("form shows success message on submit with form details", () => {});
