4394084f1e12eea96ad04dc20c3acc9d6d4c8c9f
david hays <purplehays527@gmail.com>
2020-10-02 09:29:15 -0600

    I now took out the stateful logic from the CheckoutForm.js and put it into the useForm file and it seems to be working so far

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,19 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react'
+
+export const useForm = (initialState) => {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, setValues] = useState(initialState);
+
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
+
+  return [values, showSuccessMessage, handleChanges, handleSubmit]
+
+}
